af7d2fcb62bdcf15348d61c06119175ad908e1d7
Aaron Wieldraayer <90867179+AaronWield@users.noreply.github.com>
2022-01-15 09:24:37 -0500

    4

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,11 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 from app.models import User
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
